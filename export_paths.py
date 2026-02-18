c87cd26d16461c5115ac51f2bb89ff31fd10cfc5
mzmuda101-prog <m.zmuda101@gmail.com>
2026-02-18 18:15:35 +0100

    Delete export_paths.py

--- export_paths.py
+++
@@ -1,9 +0,0 @@
-from manim import *
-
-class ExportShapes(Scene):
-    def construct(self):
-        circle = Circle()
-        star = Star(n=5, inner_radius=0.5)
-        
-        # Zamiast prosić o tekst, po prostu dodajemy je do sceny
-        self.add(circle, star)
